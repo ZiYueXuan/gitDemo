a6444fe78767b82393d7e8cede7a1ea13c905919
ZiYueXuan <671342610@qq.com>
2021-10-03 10:43:08 +0800

    the third commit to github

---
+++ git-modile-1/src/main/java/com/test/Car.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class Car {
+    public void show() {
+        System.out.println("I am a car");
+    }
+}
--- git-modile-1/src/main/java/com/test/Test.java
+++ git-modile-1/src/main/java/com/test/Test.java
@@ -2,6 +2,8 @@ package com.test;
 
 public class Test {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.show();
         for (int i=1;i<=100;i++) {
             System.out.println("Hello git!!!");
         }
